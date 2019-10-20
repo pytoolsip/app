c320d3e9203c22a81e04553972966bbaeec7d509
JDreamHeart <15602291936@163.com>
2019-10-20 23:39:12 +0800

    feat:新增uni-app基本结构

---
+++ pages/tool/index.vue
@@ -0,0 +1,61 @@
+<template>
+	<div>
+		<view class="status_bar">
+		</view>
+		<view class="content">
+			<image class="logo" src="/static/img/logo.png"></image>
+			<view class="text-area">
+				<text class="title">{{title}}</text>
+			</view>
+		</view>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'PyToolsIP'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.status_bar {
+		height: var(--status-bar-height);
+		width: 100%;
+	}
+	
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
